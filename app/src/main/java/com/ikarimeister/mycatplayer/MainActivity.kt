934493c4c62eb492ce3cb70fb4a718c2aff81dd1
Juan Carlos Garcia <jcgseco@gmail.com>
2020-04-15 21:57:43 +0200

    Second exercise ✏️ Modify a text from a textview by code using variables

--- app/src/main/java/com/ikarimeister/mycatplayer/MainActivity.kt
+++ app/src/main/java/com/ikarimeister/mycatplayer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ikarimeister.mycatplayer
 
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,7 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toast("Hello World!!!")
+        val message = "Hello Kotlin!!!"
+        val textview: TextView = findViewById(R.id.message)
+        textview.text = message
+        toast(message)
     }
 
     private fun toast(message: String) {
