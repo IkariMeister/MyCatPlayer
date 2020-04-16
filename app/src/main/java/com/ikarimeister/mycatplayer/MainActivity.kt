efa508cd96e93ec015821e6402210986d28b37be
Juan Carlos Garcia <jcgseco@gmail.com>
2020-04-16 09:22:43 +0200

    Third exercise ⏱ Get text from an EditText and add "Hello " + that text in a toast using StringTemplate

--- app/src/main/java/com/ikarimeister/mycatplayer/MainActivity.kt
+++ app/src/main/java/com/ikarimeister/mycatplayer/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.ikarimeister.mycatplayer
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,10 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val message = "Hello Kotlin!!!"
-        val textview: TextView = findViewById(R.id.message)
-        textview.text = message
-        toast(message)
+        val button: Button = findViewById(R.id.button)
+        val message: EditText = findViewById(R.id.message)
+        button.setOnClickListener { toast("Hello ${message.text}") }
     }
 
     private fun toast(message: String) {
