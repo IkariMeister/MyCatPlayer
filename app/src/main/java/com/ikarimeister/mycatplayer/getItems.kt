7c191cbb8a935be1b7b03e42a3454fd2aeabf5e8
Juan Carlos Garcia <jcgseco@gmail.com>
2020-04-23 21:56:28 +0200

    Eighteenth exercise
    Remove hardcoded creation of items.

--- app/src/main/java/com/ikarimeister/mycatplayer/getItems.kt
+++ app/src/main/java/com/ikarimeister/mycatplayer/getItems.kt
@@ -1,14 +1,9 @@
 package com.ikarimeister.mycatplayer
 
-fun getItems(): List<MediaItem> = listOf(
-        MediaItem("Title 1", "https://placekitten.com/200/200?image=1", MediaType.VIDEO),
-        MediaItem("Title 2", "https://placekitten.com/200/200?image=2", MediaType.VIDEO),
-        MediaItem("Title 3", "https://placekitten.com/200/200?image=3", MediaType.VIDEO),
-        MediaItem("Title 4", "https://placekitten.com/200/200?image=4", MediaType.VIDEO),
-        MediaItem("Title 5", "https://placekitten.com/200/200?image=5", MediaType.VIDEO),
-        MediaItem("Title 6", "https://placekitten.com/200/200?image=6", MediaType.PHOTO),
-        MediaItem("Title 7", "https://placekitten.com/200/200?image=7", MediaType.PHOTO),
-        MediaItem("Title 8", "https://placekitten.com/200/200?image=8", MediaType.PHOTO),
-        MediaItem("Title 9", "https://placekitten.com/200/200?image=9", MediaType.PHOTO),
-        MediaItem("Title 10", "https://placekitten.com/200/200?image=10", MediaType.PHOTO)
-)
+fun getItems(count: Int = 10): List<MediaItem> =
+        (1..count).map {
+            MediaItem("Title $it", "https://placekitten.com/200/200?image=$it", when (it % 2) {
+                0 -> MediaType.VIDEO
+                else -> MediaType.PHOTO
+            })
+        }
