36b0612b4a5d43e7b00464b93a593676bf384aeb
Juan Carlos Garcia <jcgseco@gmail.com>
2020-04-23 21:17:38 +0200

    Seventeenth exercise
    Create a filter menu to show Videos, Photos or All.

--- app/src/main/java/com/ikarimeister/mycatplayer/MainActivity.kt
+++ app/src/main/java/com/ikarimeister/mycatplayer/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ikarimeister.mycatplayer
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.ikarimeister.mycatplayer.databinding.ActivityMainBinding
 
@@ -13,5 +15,22 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.recycler.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        adapter.items = getItems().let { media ->
+            when (item.itemId) {
+                R.id.filter_all -> media
+                R.id.filter_photos -> media.filter { it.type == MediaType.PHOTO }
+                R.id.filter_videos -> media.filter { it.type == MediaType.VIDEO }
+                else -> media
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
