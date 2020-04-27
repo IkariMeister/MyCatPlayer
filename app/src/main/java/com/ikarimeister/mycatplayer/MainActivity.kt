9648dd05e9def69076e03aa46f7208c13a5e92d7
Juan Carlos Garcia <jcgseco@gmail.com>
2020-04-27 22:47:38 +0200

    Twelfth exercise
    Add Coroutines to getItems to run in background with a block of 2 secs.

--- app/src/main/java/com/ikarimeister/mycatplayer/MainActivity.kt
+++ app/src/main/java/com/ikarimeister/mycatplayer/MainActivity.kt
@@ -3,18 +3,31 @@ package com.ikarimeister.mycatplayer
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import com.ikarimeister.mycatplayer.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val adapter by lazy { MediaItemAdapter(MediaProvider.getItems()) { toast(it.title) } }
+    private val adapter by lazy { MediaItemAdapter { toast(it.title) } }
+
+    private fun loadData(f: (List<MediaItem>) -> Unit) = GlobalScope.launch(Dispatchers.Main) {
+        binding.progress.visibility = View.VISIBLE
+        val elements = withContext(Dispatchers.IO) { async { MediaProvider.getItems() } }
+        f(elements.await())
+        binding.progress.visibility = View.GONE
+    }
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.recycler.adapter = adapter
+        loadData { adapter.items = it }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,13 +35,16 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    @UiThread
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        adapter.items = MediaProvider.getItems().let { media ->
-            when (item.itemId) {
-                R.id.filter_all -> media
-                R.id.filter_photos -> media.filter { it.type == MediaType.PHOTO }
-                R.id.filter_videos -> media.filter { it.type == MediaType.VIDEO }
-                else -> media
+        loadData { items ->
+            adapter.items = items.let { media ->
+                when (item.itemId) {
+                    R.id.filter_all -> media
+                    R.id.filter_photos -> media.filter { it.type == MediaType.PHOTO }
+                    R.id.filter_videos -> media.filter { it.type == MediaType.VIDEO }
+                    else -> media
+                }
             }
         }
         return super.onOptionsItemSelected(item)
--- app/src/main/java/com/ikarimeister/mycatplayer/MediaProvider.kt
+++ app/src/main/java/com/ikarimeister/mycatplayer/MediaProvider.kt
@@ -1,11 +1,15 @@
 package com.ikarimeister.mycatplayer
 
 object MediaProvider {
-    fun getItems(count: Int = 10): List<MediaItem> =
-            (1..count).map {
-                MediaItem("Title $it", "https://placekitten.com/200/200?image=$it", when (it % 2) {
-                    0 -> MediaType.VIDEO
-                    else -> MediaType.PHOTO
-                })
-            }
+    const val timeSleep: Long = 2000
+
+    suspend fun getItems(count: Int = 10): List<MediaItem> {
+        Thread.sleep(timeSleep)
+        return (1..count).map {
+            MediaItem("Title $it", "https://placekitten.com/200/200?image=$it", when (it % 2) {
+                0 -> MediaType.VIDEO
+                else -> MediaType.PHOTO
+            })
+        }
+    }
 }
